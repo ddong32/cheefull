01f591dcacedc7e762e87bc5c5489c32c1834b31
ddong32 <qin@DESKTOP-2I26VIR>
2017-12-09 14:37:12 +0800

    反编译 去掉 /**/

--- src/com/chee/service/impl/CooperatorServiceImpl.java
+++ src/com/chee/service/impl/CooperatorServiceImpl.java
@@ -1,34 +1,23 @@
-/*  1:   */ package com.chee.service.impl;
-/*  2:   */ 
-/*  3:   */ import com.chee.common.Page;
-/*  4:   */ import com.chee.dao.CooperatorDao;
-/*  5:   */ import com.chee.entity.Cooperator;
-/*  6:   */ import com.chee.service.CooperatorService;
-/*  7:   */ import javax.annotation.Resource;
-/*  8:   */ import org.springframework.stereotype.Service;
-/*  9:   */ 
-/* 10:   */ @Service
-/* 11:   */ public class CooperatorServiceImpl
-/* 12:   */   extends BaseServiceImpl<Cooperator, Integer>
-/* 13:   */   implements CooperatorService
-/* 14:   */ {
-/* 15:   */   @Resource
-/* 16:   */   CooperatorDao cooperatorDao;
-/* 17:   */   
-/* 18:   */   @Resource
-/* 19:   */   public void setBaseDao(CooperatorDao cooperatorDao)
-/* 20:   */   {
-/* 21:19 */     super.setBaseDao(cooperatorDao);
-/* 22:   */   }
-/* 23:   */   
-/* 24:   */   public Page<Cooperator> findCooperatorDialogPage(Page<Cooperator> page, Cooperator cooperator)
-/* 25:   */   {
-/* 26:23 */     return this.cooperatorDao.findCooperatorDialogPage(page, cooperator);
-/* 27:   */   }
-/* 28:   */ }
+package com.chee.service.impl;
 
-
-/* Location:           C:\Users\asus\Desktop\full\WEB-INF\classes\
- * Qualified Name:     com.chee.service.impl.CooperatorServiceImpl
- * JD-Core Version:    0.7.0.1
- */
+import com.chee.common.Page;
+import com.chee.dao.CooperatorDao;
+import com.chee.entity.Cooperator;
+import com.chee.service.CooperatorService;
+import javax.annotation.Resource;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CooperatorServiceImpl extends BaseServiceImpl<Cooperator, Integer> implements CooperatorService {
+    @Resource
+    CooperatorDao cooperatorDao;
+
+    @Resource
+    public void setBaseDao(CooperatorDao cooperatorDao) {
+        super.setBaseDao(cooperatorDao);
+    }
+
+    public Page<Cooperator> findCooperatorDialogPage(Page<Cooperator> page, Cooperator cooperator) {
+        return this.cooperatorDao.findCooperatorDialogPage(page, cooperator);
+    }
+}
--- src/com/chee/service/impl/CustomerServiceImpl.java
+++ src/com/chee/service/impl/CustomerServiceImpl.java
@@ -1,46 +1,33 @@
-/*  1:   */ package com.chee.service.impl;
-/*  2:   */ 
-/*  3:   */ import com.chee.common.Page;
-/*  4:   */ import com.chee.dao.CustomerDao;
-/*  5:   */ import com.chee.entity.Customer;
-/*  6:   */ import com.chee.service.CustomerService;
-/*  7:   */ import java.util.List;
-/*  8:   */ import java.util.Set;
-/*  9:   */ import javax.annotation.Resource;
-/* 10:   */ import org.springframework.stereotype.Service;
-/* 11:   */ 
-/* 12:   */ @Service
-/* 13:   */ public class CustomerServiceImpl
-/* 14:   */   extends BaseServiceImpl<Customer, Integer>
-/* 15:   */   implements CustomerService
-/* 16:   */ {
-/* 17:   */   @Resource
-/* 18:   */   CustomerDao customerDao;
-/* 19:   */   
-/* 20:   */   @Resource
-/* 21:   */   public void setBaseDao(CustomerDao customerDao)
-/* 22:   */   {
-/* 23:21 */     super.setBaseDao(customerDao);
-/* 24:   */   }
-/* 25:   */   
-/* 26:   */   public Page<Customer> findPage(Page<Customer> page, Customer customer)
-/* 27:   */   {
-/* 28:25 */     return this.customerDao.findPage(page, customer);
-/* 29:   */   }
-/* 30:   */   
-/* 31:   */   public List<Customer> getCustomerList(Integer grade)
-/* 32:   */   {
-/* 33:29 */     return this.customerDao.getCustomerList(grade);
-/* 34:   */   }
-/* 35:   */   
-/* 36:   */   public List<Customer> getPathCustomerList(Set<Integer> paramSet)
-/* 37:   */   {
-/* 38:33 */     return this.customerDao.getPathCustomerList(paramSet);
-/* 39:   */   }
-/* 40:   */ }
+package com.chee.service.impl;
 
-
-/* Location:           C:\Users\asus\Desktop\full\WEB-INF\classes\
- * Qualified Name:     com.chee.service.impl.CustomerServiceImpl
- * JD-Core Version:    0.7.0.1
- */
+import com.chee.common.Page;
+import com.chee.dao.CustomerDao;
+import com.chee.entity.Customer;
+import com.chee.service.CustomerService;
+import java.util.List;
+import java.util.Set;
+import javax.annotation.Resource;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerServiceImpl extends BaseServiceImpl<Customer, Integer> implements CustomerService {
+    @Resource
+    CustomerDao customerDao;
+
+    @Resource
+    public void setBaseDao(CustomerDao customerDao) {
+        super.setBaseDao(customerDao);
+    }
+
+    public Page<Customer> findPage(Page<Customer> page, Customer customer) {
+        return this.customerDao.findPage(page, customer);
+    }
+
+    public List<Customer> getCustomerList(Integer grade) {
+        return this.customerDao.getCustomerList(grade);
+    }
+
+    public List<Customer> getPathCustomerList(Set<Integer> paramSet) {
+        return this.customerDao.getPathCustomerList(paramSet);
+    }
+}
